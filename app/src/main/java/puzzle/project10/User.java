269da683471bc867f3f3454f2685cc188e5c733a
lim xiang <xiang_lim@hotmal.com>
2016-08-03 01:38:58 +0800

    mansour

---
+++ app/src/main/java/puzzle/project10/User.java
@@ -0,0 +1,26 @@
+package puzzle.project10;
+
+/**
+ * Created by lim xiang on 02-Aug-16.
+ */
+public class User {
+    String name, username, password;
+    int age;
+
+    public User(String name, int age, String username, String password){
+        this.name = name;
+        this.age = age;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+        this.age = -1;
+        this.name = "";
+
+
+    }
+}
+
